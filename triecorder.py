2c0ab8ad81b3cf704a681b3d01a9cf28f549d610
Andrew Matteson <amattteson3@gmail.com>
2020-06-05 19:07:04 -0400

    Added support for a custom delimiter

--- triecorder.py
+++ triecorder.py
@@ -8,13 +8,48 @@ DEFAULT_FANOUT_THRESHOLD = 0.5
 DEFAULT_MIN_COUNT = 3
 DEFAULT_MULTIPLIER = 0.33
 
+class SuperString:
+    def __init__(self, parts=[], delimiter=''):
+        self.parts = parts
+        self.delimiter = delimiter
+
+    def startswith(self, other):
+        for i in range(len(other.parts)):
+            if self.parts[i] != other.parts[i]:
+                return False
+        return True
+
+    def __getitem__(self, i):
+        if isinstance(i, slice):
+            return SuperString(self.parts[i], self.delimiter)
+        else:
+            return self.parts[i]
+
+    def __hash__(self):
+        return hash(tuple(self.parts)) & hash(self.delimiter)
+
+    def __len__(self):
+        return len(self.parts)
+
+    def __eq__(self, other):
+        return hash(self) == hash(other)
+
+    @classmethod
+    def from_string(self, string, delimiter):
+        return SuperString(string.split(delimiter), delimiter)
+
+    def __repr__(self):
+        return self.delimiter.join(self.parts)
+
 class Trie:
-    def __init__(self, string):
+    def __init__(self, string=None):
         self.string = string
         self.children = {}
         self.count = 0
 
     def contains(self, string):
+        if self.string is None:
+            return False
         if self.string == string:
             return True
         if self.string.startswith(string):
@@ -30,21 +65,22 @@ class Trie:
     def find_child(self, string):
         return self.children.get(string[0])
 
-    def add_child(self, trie):
+    def _add_child(self, trie):
         self.children[trie.string[0]] = trie
         self.count += 1
 
-    def split_at(self, i):
+    def _split_at(self, i):
         heir = Trie(self.string[i:])
         heir.children = self.children
         heir.count = self.count
         self.children = {}
-        self.add_child(heir)
+        self._add_child(heir)
         self.string = self.string[:i]
 
     def add(self, new_str):
-        assert(isinstance(new_str, str))
-        num_added = 0
+        if self.string is None:
+            c = self.string = type(new_str)()
+        num_added = 0 # for updating counts
         for i in range(len(new_str)):
             if i == len(self.string):
                 rest = new_str[len(self.string):]
@@ -53,12 +89,12 @@ class Trie:
                     num_added = c.add(rest)
                     self.count += num_added
                 else:
-                    self.add_child(Trie(rest))
+                    self._add_child(Trie(rest))
                     num_added = 1
                 break
             if self.string[i] != new_str[i]:
-                self.split_at(i)
-                self.add_child(Trie(new_str[i:]))
+                self._split_at(i)
+                self._add_child(Trie(new_str[i:]))
                 num_added = 2
                 break
         return num_added
@@ -76,11 +112,14 @@ class Trie:
 
     def summarize(self, fanout_threshold, min_count, prefix=''):
         """Nodes with high fanout are truncated."""
-        prefix = prefix + self.string
+        delimiter = ''
+        if isinstance(self.string, SuperString) and len(self.children) > 0:
+            delimiter = self.string.delimiter
+        prefix = prefix + str(self.string) + delimiter
         if len(self.children) == 0:
             return prefix
         if self.count > min_count and self.string != '' and self.fanout >= fanout_threshold:
-            return prefix + '... (%s)' % self.count
+            return prefix + ' ... (%s)' % self.count
         return '\n'.join(c.summarize(fanout_threshold, min_count, prefix)
                          for c in self.children.values())
 
@@ -92,9 +131,8 @@ def median(items):
 
 def main(args):
     parser = argparse.ArgumentParser(description="""Summarize lines of input.""")
-    parser.add_argument('-d', '--debug', dest='debug', action='store_true',
-                        help='Print debug output')
-    debug = os.environ.get('DEBUG', '').lower() == 'true'
+    parser.add_argument('-v', '--verbose', dest='verbose', action='store_true',
+                        help='Print trie structure and stats')
     parser.add_argument('-m', dest='min_count', type=int, default=None,
         help='Minimum total child node count to qualify a node for summarization. ')
     parser.add_argument('-t', dest='fanout_threshold', type=float, default=None,
@@ -103,25 +141,28 @@ def main(args):
     parser.add_argument('-M', dest='multiplier', type=float, default=DEFAULT_MULTIPLIER,
         help='Multiplier used to automatically determine summarization parameters. ' +
              'Increase to show more values. Default: %s' % DEFAULT_MULTIPLIER)
+    parser.add_argument('-d', dest='delimiter', type=str,
+        help='Delimiter. Default is None (nodes can split from any letter).')
     opts = parser.parse_args(args)
 
-    trie = Trie('')
+    trie = Trie()
     for line in sys.stdin.readlines():
-        trie.add(line.strip())
+        s = SuperString.from_string(line.strip(), opts.delimiter) if opts.delimiter else line.strip()
+        trie.add(s)
 
-    if opts.debug:
+    if opts.verbose:
         print(trie)
 
     if opts.fanout_threshold is None:
-        opts.fanout_threshold = median([c.fanout for c in trie.children.values()
-                                        if c.fanout < 1]) * opts.multiplier
+        fanouts = [c.fanout for c in trie.children.values() if c.fanout < 1]
+        opts.fanout_threshold = median(fanouts) * opts.multiplier
 
     if opts.min_count is None:
         opts.min_count = int(median([c.count for c in trie.children.values()]) * opts.multiplier)
 
     print(trie.summarize(opts.fanout_threshold, opts.min_count))
 
-    if opts.debug:
+    if opts.verbose:
         print('\n(fanout_threshold: %s, min_count: %s)' %
                 (opts.fanout_threshold, opts.min_count))
 
